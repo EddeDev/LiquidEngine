9ae6ccb58216c056808c29e2bf6723d348937b9d
Edvin Pettersson <eddedev@hotmail.com>
2022-11-18 23:10:45 +0100

    Update WindowsLaunch.cpp

--- Liquid/Source/Liquid/Launch/Windows/WindowsLaunch.cpp
+++ Liquid/Source/Liquid/Launch/Windows/WindowsLaunch.cpp
@@ -13,22 +13,22 @@ namespace Liquid {
 }
 
 #ifdef LQ_BUILD_SHIPPING
-#ifdef _UNICODE
-int WINAPI wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prevInstance, _In_ PWSTR cmdLine, _In_ int showCmd)
-{
-	return Liquid::Main({ __argc, __argv });
-}
+	#ifdef _UNICODE
+		int WINAPI wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prevInstance, _In_ PWSTR cmdLine, _In_ int showCmd)
+		{
+			return Liquid::Main({ __argc, __argv });
+		}
+	#else
+		int WINAPI WinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prevInstance, _In_ PSTR cmdLine, _In_ int showCmd)
+		{
+			return Liquid::Main({ __argc, __argv });
+		}
+	#endif
 #else
-int WINAPI WinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prevInstance, _In_ PSTR cmdLine, _In_ int showCmd)
-{
-	return Liquid::Main({ __argc, __argv });
-}
-#endif
-#else
-int main(int argc, char* argv[])
-{
-	return Liquid::Main({ argc, argv });
-}
+	int main(int argc, char* argv[])
+	{
+		return Liquid::Main({ argc, argv });
+	}
 #endif
 
 #endif
